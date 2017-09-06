b01f80bcfba0cfcf516e3c48abbeb4a8c0a22024
tzydo <zydorowiczt@gmail.com>
2017-09-06 20:31:16 +0200

    hibernate configuration

--- SkiJumpClassyficationPage/src/main/java/com/pl/ski_jumping/config/Configurations.java
+++ SkiJumpClassyficationPage/src/main/java/com/pl/ski_jumping/config/Configurations.java
@@ -1,24 +1,17 @@
 package com.pl.ski_jumping.config;
 
-import org.apache.commons.dbcp.BasicDataSource;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 
 @Configuration
-@EnableTransactionManagement
-
 public class Configurations {
 
     @Autowired
@@ -35,16 +28,6 @@ public class Configurations {
         return driverManagerDataSource;
     }
 
-    @Bean
-    public LocalSessionFactoryBean localSessionFactoryBean(){
-        final LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan(new String[]{"com.pl.ski_jumping.model"});
-        sessionFactory.setHibernateProperties(hibernateProperties());
-
-        return sessionFactory;
-    }
-
     final Properties hibernateProperties(){
         final Properties hibProperties = new Properties();
         hibProperties.setProperty("hibernate.hbm2ddl.auto",environment.getProperty("hibernate.hbm2ddl.auto"));
@@ -54,17 +37,22 @@ public class Configurations {
         return hibProperties;
     }
 
+//
     @Bean
-    @Autowired
-    public HibernateTransactionManager transactionManager(final SessionFactory sessionFactory){
-        final HibernateTransactionManager hibernateTransactio = new HibernateTransactionManager();
-        hibernateTransactio.setSessionFactory(sessionFactory);
+    public LocalContainerEntityManagerFactoryBean managerFactoryBean(){
+        LocalContainerEntityManagerFactoryBean managerFactoryBean
+                = new LocalContainerEntityManagerFactoryBean();
 
-        return hibernateTransactio;
-    }
+        managerFactoryBean.setDataSource(dataSource());
+        managerFactoryBean.setPackagesToScan(new String[]{"com.pl.ski_jumping"});
 
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslationPostProcessor(){
-        return new PersistenceExceptionTranslationPostProcessor();
+
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+//        Vendora adapter
+        managerFactoryBean.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+//        Hibernate properties
+        managerFactoryBean.setJpaProperties(hibernateProperties());
+
+        return managerFactoryBean;
     }
 }
