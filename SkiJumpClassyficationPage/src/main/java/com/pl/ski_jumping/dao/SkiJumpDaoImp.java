8a2d491b25b9bb6d00c6502c5a009a7deac99c6e
tzydo <zydorowiczt@gmail.com>
2017-09-06 20:28:46 +0200

    dao, repository

---
+++ SkiJumpClassyficationPage/src/main/java/com/pl/ski_jumping/dao/SkiJumpDaoImp.java
@@ -0,0 +1,55 @@
+package com.pl.ski_jumping.dao;
+
+import com.pl.ski_jumping.model.SkiJumper;
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+
+@Repository
+public class SkiJumpDaoImp implements SkiJumperDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public List<SkiJumper> findAll() {
+        return (List<SkiJumper>) entityManager.createQuery("from SkiJumper").getResultList();
+    }
+
+    @Override
+    public SkiJumper findByRank(int rank) {
+        return (SkiJumper) entityManager.createQuery("from SkiJumper where rank=:rank")
+                                        .setParameter("rank", rank)
+                                        .getSingleResult();
+    }
+
+    @Override
+    public List<SkiJumper> findByName(String name) {
+        return (List<SkiJumper>) entityManager.createQuery("from SkiJumper where name =:name ")
+                                                .setParameter("name", name)
+                                                .getResultList();
+    }
+
+    @Override
+    public void save(SkiJumper skiJumper) {
+        entityManager.persist(skiJumper);
+    }
+
+    public void update(SkiJumper skiJumper){
+        entityManager.merge(skiJumper);
+    }
+
+    @Override
+    public void deleteByRank(int rank) {
+        entityManager.remove(findByRank(rank));
+    }
+
+    @Override
+    public void deleteAll() {
+        Query query = entityManager.createNativeQuery("TRUNCATE TABLE SkiJumper");
+        query.executeUpdate();
+    }
+}
---
+++ SkiJumpClassyficationPage/src/main/java/com/pl/ski_jumping/dao/SkiJumperDao.java
@@ -0,0 +1,16 @@
+package com.pl.ski_jumping.dao;
+
+import com.pl.ski_jumping.model.SkiJumper;
+
+import java.util.List;
+
+public interface SkiJumperDao{
+
+    List<SkiJumper> findAll();
+    SkiJumper findByRank(int rank);
+    List<SkiJumper> findByName(String name);
+    void save(SkiJumper skiJumper);
+    void update(SkiJumper skiJumper);
+    void deleteByRank(int rank);
+    void deleteAll();
+}
