653c2d7e62cf690265bc5b3d5cb05a648b00b45f
tzydo <zydorowiczt@gmail.com>
2017-09-11 20:38:59 +0200

    front add basic routing

--- SkiJumpClassyficationPage/src/main/resources/front/src/app/app.module.ts
+++ SkiJumpClassyficationPage/src/main/resources/front/src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router'
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './home/app.component';
+import {SearchComponent} from './search/search.component';
+import {ClassificationComponent} from "./classification/classification.component";
+import {AddJumper} from "./add/addJumper.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ClassificationComponent,
+    AddJumper
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        component: AppComponent
+      }, {
+        path: 'search',
+        component: SearchComponent
+      }, {
+        path: 'classification',
+        component: ClassificationComponent
+      }, {
+        path: 'add',
+        component: AddJumper
+      }, {
+        path: ' ', redirectTo: '/home', pathMatch: 'full'
+      }, {
+        path: '**', redirectTo: 'home', pathMatch: 'full'
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ SkiJumpClassyficationPage/src/main/resources/front/src/app/classification/classification.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'app-classification',
+  templateUrl: './classification.component.html',
+})
+
+export class ClassificationComponent{}
