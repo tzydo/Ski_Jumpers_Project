de3a2419c28384eca874100b2a0a749c75431f66
tzydo <zydorowiczt@gmail.com>
2017-09-11 19:38:06 +0200

    front configuration, adding bootstrap

--- SkiJumpClassyficationPage/src/main/java/com/pl/ski_jumping/controllers/HomeController.java
+++ SkiJumpClassyficationPage/src/main/java/com/pl/ski_jumping/controllers/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "home")
+@RequestMapping(value = "api")
 public class HomeController {
 
     @Autowired
