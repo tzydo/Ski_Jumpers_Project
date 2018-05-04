3dd77d937b00920e53fe3c874ff0344735e196b5
tzydo <zydorowiczt@gmail.com>
2018-05-05 00:20:49 +0200

    adding batch project

---
+++ ski_jumping.batch/src/main/java/com/pl/skijumping/batch/BatchApplication.java
@@ -0,0 +1,17 @@
+package com.pl.skijumping.batch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableBatchProcessing
+@SpringBootApplication
+@EnableScheduling
+@EnableJpaRepositories
+public class BatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+}
