01650518c6fe85b6d342dfeff407e836cae1d494
tzydo <zydorowiczt@gmail.com>
2018-06-05 20:05:52 +0200

    add query dsl to project

--- ski_jumping.domain/src/main/java/com/pl/skijumping/domain/repository/CompetitionTypeRepository.java
+++ ski_jumping.domain/src/main/java/com/pl/skijumping/domain/repository/CompetitionTypeRepository.java
@@ -2,8 +2,9 @@ package com.pl.skijumping.domain.repository;
 
 import com.pl.skijumping.domain.entity.CompetitionType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompetitionTypeRepository extends JpaRepository<CompetitionType, Long> {
+public interface CompetitionTypeRepository extends JpaRepository<CompetitionType, Long>, QueryDslPredicateExecutor {
 }
