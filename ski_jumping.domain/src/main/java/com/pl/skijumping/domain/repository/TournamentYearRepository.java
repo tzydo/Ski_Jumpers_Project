7e009d4ede65881aebb2522c9908a4d58c9ef9d4
tzydo <zydorowiczt@gmail.com>
2018-08-26 12:18:22 +0200

    change behavior repository from using limit and top to pageable

--- ski_jumping.domain/src/main/java/com/pl/skijumping/domain/repository/TournamentYearRepository.java
+++ ski_jumping.domain/src/main/java/com/pl/skijumping/domain/repository/TournamentYearRepository.java
@@ -1,9 +1,8 @@
 package com.pl.skijumping.domain.repository;
 
 import com.pl.skijumping.domain.entity.TournamentYear;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +11,5 @@ import java.util.List;
 public interface TournamentYearRepository extends JpaRepository<TournamentYear, Long> {
     TournamentYear findByYear(String year);
 
-    @Query(nativeQuery = true, value = "SELECT Top :limit * FROM Tournament_Year ORDER BY year DESC")
-    List<TournamentYear> findAllByTopAndLimit(@Param("limit") Integer limit);
+    List<TournamentYear> findAllByOrderByYearDesc(Pageable limit);
 }
