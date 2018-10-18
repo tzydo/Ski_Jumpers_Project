376dfe405170b1958e6dc837d3247c20d12b3a99
tzydo <zydorowiczt@gmail.com>
2018-10-18 18:37:03 +0200

    fix bug "Cannot parse "DATE" constant "ace..."; SQL statement:"

--- ski_jumping.service/src/test/java/com/pl/skijumping/service/ApplicationTest.java
+++ ski_jumping.service/src/test/java/com/pl/skijumping/service/ApplicationTest.java
@@ -4,11 +4,12 @@ import org.junit.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@EntityScan(basePackages = "com.pl.skijumping.domain.entity")
+@EntityScan(basePackages = "com.pl.skijumping.domain.entity", basePackageClasses = {Jsr310JpaConverters.class})
 @EnableJpaRepositories(basePackages = "com.pl.skijumping.domain.*")
 @SpringBootApplication(scanBasePackages = "com.pl.skijumping.*")
 @ComponentScan(basePackages = "com.pl.skijumping")
