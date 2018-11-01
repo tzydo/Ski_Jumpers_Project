3492c69837b13032ed5f1fb8e70e863807cd5b59
zyzio <zydorowiczt@gmail.com>
2018-11-01 18:46:19 +0100

    adding rest module

---
+++ ski_jumping.rest/src/main/java/com/pl/skijumping/rest/ApplicationRest.java
@@ -0,0 +1,12 @@
+package com.pl.skijumping.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRest {
+
+    public static void main(String... args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+}
