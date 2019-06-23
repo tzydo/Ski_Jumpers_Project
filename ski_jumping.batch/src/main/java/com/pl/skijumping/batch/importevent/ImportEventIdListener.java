746a978abaa5ea4fc154cc09f7c44ae43017d2fa
zyzio <zydorowiczt@gmail.com>
2019-06-23 17:46:18 +0200

    add import event listener with tests

---
+++ ski_jumping.batch/src/main/java/com/pl/skijumping/batch/importevent/ImportEventIdListener.java
@@ -0,0 +1,77 @@
+package com.pl.skijumping.batch.importevent;
+
+import com.pl.skijumping.batch.matchingword.MatchingWords;
+import com.pl.skijumping.batch.reader.DataReader;
+import com.pl.skijumping.batch.util.FileScannerConst;
+import com.pl.skijumping.diagnosticmonitor.DiagnosticMonitor;
+import com.pl.skijumping.dto.MessageDTO;
+import com.pl.skijumping.dto.MessagePropertiesConst;
+import com.pl.skijumping.rabbitmq.producer.RabbitmqProducer;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ImportEventIdListener {
+
+    private final DiagnosticMonitor diagnosticMonitor;
+    private final String eventHost;
+    private final String sourceImportEventListener;
+    private final String importDataRaceEventListener;
+    private final String exchange;
+    private final RabbitmqProducer rabbitmqProducer;
+
+    public ImportEventIdListener(DiagnosticMonitor diagnosticMonitor,
+                                 @Value("${skijumping.settings.eventHost}") String eventHost,
+                                 @Value("${skijumping.rabbitmq.queues.sourceImportEventListener}") String sourceImportEventListener,
+                                 @Value("${skijumping.rabbitmq.queues.importDataRaceEventListener}") String importDataRaceEventListener,
+                                 @Value("${skijumping.rabbitmq.exchange}") String exchange,
+                                 RabbitmqProducer rabbitmqProducer) {
+        this.diagnosticMonitor = diagnosticMonitor;
+        this.eventHost = eventHost;
+        this.sourceImportEventListener = sourceImportEventListener;
+        this.importDataRaceEventListener = importDataRaceEventListener;
+        this.exchange = exchange;
+        this.rabbitmqProducer = rabbitmqProducer;
+    }
+
+    @RabbitListener(bindings =
+    @QueueBinding(
+            value = @Queue(value = "${skijumping.rabbitmq.queues.importEventIdListener}", durable = "true"),
+            exchange = @Exchange(value = "${skijumping.rabbitmq.exchange}", type = ExchangeTypes.TOPIC, durable = "true")
+    ))
+    public void importEvent(MessageDTO messageDTO) {
+        if (messageDTO == null) {
+            return;
+        }
+        Integer seasonCode = messageDTO.getProperties().getIntValue(MessagePropertiesConst.SEASON_CODE.getValue());
+        if(seasonCode == null) {
+            diagnosticMonitor.logWarn("Season code cannot be null");
+            return;
+        }
+        DataReader dataReader = new DataReader(diagnosticMonitor);
+        String readText = dataReader.read(messageDTO.getFilePath());
+        if (readText == null || readText.isEmpty()) {
+            diagnosticMonitor.logWarn("Cannot read event id from empty file");
+            return;
+        }
+        MatchingWords matchingWords = new MatchingWords(diagnosticMonitor);
+        matchingWords.getEventIds(readText)
+                .forEach(event -> sendMessage(event, seasonCode));
+    }
+
+    private void sendMessage(String eventId, Integer seasonCode) {
+        String url = String.format(eventHost, eventId, seasonCode);
+        MessageDTO messageDTO = new MessageDTO()
+                .addProperties(MessagePropertiesConst.DESTINATION_TARGET.getValue(), importDataRaceEventListener)
+                .addProperties(MessagePropertiesConst.EVENT_ID.getValue(), eventId)
+                .addProperties(MessagePropertiesConst.SEASON_CODE.getValue(), seasonCode)
+                .addProperties(MessagePropertiesConst.FILE_NAME.getValue(), FileScannerConst.prepareFileName(FileScannerConst.FILE_DATA_IMPORT, seasonCode + "_" + eventId))
+                .addProperties(MessagePropertiesConst.DOWNLOAD_SOURCE_URL.getValue(), url);
+        rabbitmqProducer.sendMessage(exchange, sourceImportEventListener, messageDTO);
+    }
+}
---
+++ ski_jumping.batch/src/test/java/com/pl/skijumping/batch/importevent/ImportEventIdListenerTest.java
@@ -0,0 +1,40 @@
+package com.pl.skijumping.batch.importevent;
+
+import com.pl.skijumping.batch.SetupUtilTests;
+import com.pl.skijumping.diagnosticmonitor.DiagnosticMonitor;
+import com.pl.skijumping.dto.MessageDTO;
+import com.pl.skijumping.dto.MessagePropertiesConst;
+import com.pl.skijumping.rabbitmq.producer.RabbitmqProducer;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class ImportEventIdListenerTest {
+
+    private static final String HOST = "host";
+    private static final String SOURCE_IMPORT_EVENT_LISTENER = "sourceImportEventListener";
+    private static final String IMPORT_DATA_RACE_EVENT_LISTENER = "importDataRaceEventListener";
+    private static final String EXCHANGE = "exchange";
+
+    @Test
+    public void importEvent() throws IOException {
+        DiagnosticMonitor diagnosticMonitorMock = SetupUtilTests.getDiagnosticMonitorMock();
+        RabbitmqProducer rabbitmqProducer = Mockito.mock(RabbitmqProducer.class);
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(MessagePropertiesConst.SEASON_CODE.getValue(), 2018);
+        MessageDTO messageDTO = new MessageDTO().filePath(Paths.get(new ClassPathResource("import/eventIdImport.txt").getURI()).toString())
+                .properties(properties);
+
+        ImportEventIdListener importEventIdListener = new ImportEventIdListener(diagnosticMonitorMock, HOST, SOURCE_IMPORT_EVENT_LISTENER, IMPORT_DATA_RACE_EVENT_LISTENER, EXCHANGE, rabbitmqProducer);
+        importEventIdListener.importEvent(messageDTO);
+        Mockito.verify(rabbitmqProducer, Mockito.times(7)).sendMessage(Mockito.any(), Mockito.any(), Mockito.any());
+    }
+}
